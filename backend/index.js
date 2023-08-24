51873566713033a7d9a2e36587b554c496009e9a
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-08-24 13:19:11 -0500

    feat: :bug: INDEX
    se cambio un caracter invalido que generaba un error

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
-dotenv.config()
-const Express=express()
+console.clear();
+dotenv.config();
+const Express=express();
 
-Express.use(express,json())
+Express.use(express.json());
 
-const config=JSON.parse(process.env.MY_CONFIG)
-Express.listen(config,()=>{console.log(`http://${config.hostname}:${config.port}`)})
+const config=JSON.parse(process.env.MY_CONFIG);
+Express.listen(config,()=>{console.log(`http://${config.hostname}:${config.port}`)});
