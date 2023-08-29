dd79102514b2c2e9720f3459232850fec9079d3e
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-08-29 13:41:57 -0500

    feat: :sparkles: ENDPOINT Y CRUD
    creacion de enpoint his_atencion y configuracion de metodos CRUD para su posterior uso

---
+++ backend/routers/his_atencion.js
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv';
+import {Router} from 'express'
+import { conx } from '../Database/connection.js';
+
+
+dotenv.config()
+const his_atencion = Router();
+let db= await conx();
+let collection = db.collection("his_atencion")
+
+his_atencion.get("/",async ( req,res)=>{
+    try {
+    let funtion= await collection.find({}).toArray();
+    res.send(funtion)
+    } catch (error) {
+        res.send(error)
+    }
+    
+})
+
+his_atencion.post('/',async (req,res)=>{
+    try{
+        let data=req.body;
+        await collection.insertOne(data);
+        res.send(`se ah ingresado la data`)
+    }catch(Error){ 
+        res.status(400).send(Error);
+    }
+})
+
+his_atencion.delete('/', async (req,res)=>{
+    try {
+        let data = req.body
+        let id =data._id
+        let funtion = await collection.deleteOne({"_id":id},)
+        res.send(funtion)
+
+    } catch (error) {
+        res.status(400).send(Error);
+    }
+})
+
+his_atencion.put("/", async (req,res)=>{
+    let actualizaciones ={...req.body,caducidad:new Date(req.body.caducidad)};
+    let filter = parseInt(req.query.id, 10)
+try{
+    let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
+    res.send("se ah actualizado la data")  
+} catch (error) {
+    res.send(error);
+}
+})
+
+export default his_atencion;
