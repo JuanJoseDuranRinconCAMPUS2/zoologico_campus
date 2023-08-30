cc50f5c1a30f4372e746f692eca32fb72f2b559f
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-29 21:53:36 -0500

    feat: :construction: Autoincrementables y ortografia: /habitat
    Se utiliza la funcion de "increment" para manejar ids generadas automaticamente , autoincrementables para evitar problemas de compatibilidad entre ids, ademas se realiza solucion a errores menores y ortograficos

--- backend/routers/habitad.js
+++ backend/routers/habitad.js
@@ -6,12 +6,21 @@ import { conx } from '../Database/connection.js';
 dotenv.config()
 const habitad = Router();
 let db= await conx();
-let collection = db.collection("habitad")
+let collection = db.collection("habitat")
+let autoincrement = db.collection("autoincrement");
+async function  increment(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.value.sequence_value;
+}
 
 habitad.get("/",async ( req,res)=>{
     try {
-    let funtion= await collection.find({}).toArray();
-    res.send(funtion)
+        let funtion= await collection.find({}).sort({ _id : 1}).toArray();
+        res.send(funtion)
     } catch (error) {
         res.send(error)
     }
@@ -20,9 +29,10 @@ habitad.get("/",async ( req,res)=>{
 
 habitad.post('/',async (req,res)=>{
     try{
-        let data=req.body;
+        const id =  await increment("habitat");
+        let data= {_id: id, ...req.body};
         await collection.insertOne(data);
-        res.send(`se ah ingresado la data`)
+        res.send(`se ha ingresado la data`)
     }catch(Error){ 
         res.status(400).send(Error);
     }
@@ -33,7 +43,7 @@ habitad.delete('/', async (req,res)=>{
 
         let data = req.body
         let id =data._id
-        let funtion = await collection.deleteOne({"_id":id},)
+        let funtion = await collection.deleteOne({"_id":id})
         res.send(funtion)
 
     } catch (error) {
@@ -42,14 +52,14 @@ habitad.delete('/', async (req,res)=>{
 })
 
 habitad.put("/", async (req,res)=>{
-    let actualizaciones ={...req.body,caducidad:new Date(req.body.caducidad)};
+    let actualizaciones ={...req.body};
     let filter = parseInt(req.query.id, 10)
-try{
-    let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
-    res.send("se ah actualizado la data")  
-} catch (error) {
-    res.send(error);
-}
+    try{
+        let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
+        res.send("se ah actualizado la data")  
+    } catch (error) {
+        res.send(error);
+    }
 })
 
 export default habitad;
