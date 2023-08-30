d7680e2a5aa7d101adca931f62663d874718d230
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-29 21:02:41 -0500

    feat: :construction: Autoincrementables y ortografia: /animales
    Se utiliza la funcion de "increment" para manejar ids generadas automaticamente , autoincrementables para evitar problemas de compatibilidad entre ids, ademas se realiza solucion a errores menores y ortograficos

--- backend/routers/animales.js
+++ backend/routers/animales.js
@@ -7,11 +7,19 @@ dotenv.config()
 const animales = Router();
 let db= await conx();
 let collection = db.collection("animales")
-
+let autoincrement = db.collection("autoincrement");
+async function  increment(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.value.sequence_value;
+}
 
 animales.get("/",async ( req,res)=>{
     try {
-    let funtion= await collection.find({}).toArray();
+    let funtion= await collection.find({}).sort({ _id : 1}).toArray();
     res.send(funtion)
     } catch (error) {
         res.send(error)
@@ -21,9 +29,10 @@ animales.get("/",async ( req,res)=>{
 
 animales.post('/',async (req,res)=>{
     try{
-        let data=req.body;
+        const id =  await increment("animales");
+        let data= {_id: id, ...req.body};
         await collection.insertOne(data);
-        res.send(`se ah ingresado la data`)
+        res.send(`se ha ingresado la data`)
     }catch(Error){ 
         res.status(400).send(Error);
     }
@@ -33,7 +42,7 @@ animales.delete('/', async (req,res)=>{
     try {
         let data = req.body
         let id =data._id
-        let funtion = await collection.deleteOne({"_id":id},)
+        let funtion = await collection.deleteOne({"_id":id})
         res.send(funtion)
 
     } catch (error) {
@@ -42,11 +51,11 @@ animales.delete('/', async (req,res)=>{
 })
 
 animales.put("/", async (req,res)=>{
-    let actualizaciones ={...req.body,caducidad:new Date(req.body.caducidad)};
+    let actualizaciones ={...req.body};
     let filter = parseInt(req.query.id, 10)
 try{
     let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
-    res.send("se ah actualizado la data")  
+    res.send("se ha actualizado la data")  
 } catch (error) {
     res.send(error);
 }
