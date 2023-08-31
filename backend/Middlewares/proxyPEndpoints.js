4b63fc2fb5f0a219b2a259290a481d23ca602f76
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-31 13:07:39 -0500

    feat: :safety_vest: Manejo de las reglas de validacion y Alias: /animales
    Este archivo contiene la configuracion para aplicar las reglas de validacion para este endpoint, en el cual se revisa que los datos esten correctos y luego aplica alias para mantener confidencial la estructura de la coleccion y con esto guarda ese resultado en el "req.body"

--- backend/Middlewares/proxyPEndpoints.js
+++ backend/Middlewares/proxyPEndpoints.js
@@ -1,10 +1,12 @@
 import  express from 'express';
 import { vPAlimento } from '../controllers/vAlimento.js';
 import { validationResult } from 'express-validator';
+import { vPAnimales } from '../controllers/vAnimales.js';
 
 const proxyPAlimento = express();
+const proxyPAnimales = express();
 
-//proxy usado para validar los metodos put y post
+//proxy usado para validar los metodos datos de entrada de los metodos put y post en alimentos
 
 proxyPAlimento.use(vPAlimento, async(req, res, next)=>{
     try {
@@ -25,4 +27,25 @@ proxyPAlimento.use(vPAlimento, async(req, res, next)=>{
     }
 });
 
-export {proxyPAlimento}
+//proxy usado para validar los metodos datos de entrada de los metodos put y post en animales
+
+proxyPAnimales.use(vPAnimales, async(req, res, next)=>{
+    try {
+        const error = validationResult(req);
+        if(!error.isEmpty()) return res.status(400).json(error)
+        let { 
+            nombre_Animales:nombre, especie_Animales:especie, 
+            dieta_Animales:dieta ,  descripcion_Animales:descripcion,
+            id_habitat_Ani:id_Habitat
+        } = req.body
+        req.body = {
+            nombre, especie, dieta, 
+            descripcion, id_Habitat
+        }
+        next();
+    } catch (err) {
+        res.status(err.status).send(err);
+    }
+});
+
+export {proxyPAlimento, proxyPAnimales}
