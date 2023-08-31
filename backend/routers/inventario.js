f5bdd1b8cdbdb8b3ea114cc2a4dacdef73ffc56d
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-30 19:58:58 -0500

    feat: :construction: Autoincrementables y ortografia: /inventario
    Se utiliza la funcion de "increment" para manejar ids generadas automaticamente , autoincrementables para evitar problemas de compatibilidad entre ids, ademas se realiza solucion a errores menores y ortograficos

--- backend/routers/inventario.js
+++ backend/routers/inventario.js
@@ -6,11 +6,21 @@ dotenv.config()
 const inventario = Router();
 let db= await conx();
 let collection = db.collection("inventario")
+let autoincrement = db.collection("autoincrement");
+
+async function  increment(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.value.sequence_value;
+}
 
 inventario.get("/",async ( req,res)=>{
     try {
 
-    let funtion= await collection.find({}).toArray();
+    let funtion= await collection.find({}).sort({ _id : 1}).toArray();
     res.send(funtion)
     } catch (error) {
         res.send(error)
@@ -20,10 +30,10 @@ inventario.get("/",async ( req,res)=>{
 
 inventario.post('/',async (req,res)=>{
     try{
-        let data=req.body;
-
+        const id =  await increment("inventario");
+        let data= {_id: id, ...req.body, fecha_Ingreso : new Date(req.body.fecha_Ingreso)};
         await collection.insertOne(data);
-        res.send(`se ah ingresado la data`)
+        res.send(`se ha ingresado la data`)
     }catch(Error){ 
         res.status(400).send(Error);
     }
@@ -33,7 +43,7 @@ inventario.delete('/', async (req,res)=>{
     try {
         let data = req.body
         let id =data._id
-        let funtion = await collection.deleteOne({"_id":id},)
+        let funtion = await collection.deleteOne({"_id":id})
         res.send(funtion)
 
     } catch (error) {
@@ -41,14 +51,14 @@ inventario.delete('/', async (req,res)=>{
 })
 
 inventario.put("/", async (req,res)=>{
-    let actualizaciones ={...req.body,caducidad:new Date(req.body.caducidad)};
+    let actualizaciones ={...req.body, fecha_Ingreso : new Date(req.body.fecha_Ingreso)};
     let filter = parseInt(req.query.id, 10)
-try{
-    let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
-    res.send("se ah actualizado la data")  
-} catch (error) {
-    res.send(error);
-}
+    try{
+        let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
+        res.send("se ha actualizado la data")  
+    } catch (error) {
+        res.send(error);
+    }
 })
 
 export default inventario;
