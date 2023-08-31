6a233e610fce20991e6250bbf38c8c1745def7e1
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-30 20:10:33 -0500

    feat: :construction: Autoincrementables y ortografia: /medicamento
    Se utiliza la funcion de "increment" para manejar ids generadas automaticamente , autoincrementables para evitar problemas de compatibilidad entre ids, ademas se realiza solucion a errores menores y ortograficos

--- backend/routers/medicamento.js
+++ backend/routers/medicamento.js
@@ -7,10 +7,20 @@ dotenv.config()
 const medicamento = Router();
 let db= await conx();
 let collection = db.collection("medicamento")
+let autoincrement = db.collection("autoincrement");
+
+async function  increment(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.value.sequence_value;
+}
 
 medicamento.get("/",async ( req,res)=>{
     try {
-    let funtion= await collection.find({}).toArray();
+    let funtion= await collection.find({}).sort({ _id : 1}).toArray();
     res.send(funtion)
     } catch (error) {
         res.send(error)
@@ -20,9 +30,10 @@ medicamento.get("/",async ( req,res)=>{
 
 medicamento.post('/',async (req,res)=>{
     try{
-        let data=req.body;
+        const id =  await increment("medicamento");
+        let data= {_id: id, ...req.body, caducidad : new Date(req.body.caducidad)};
         await collection.insertOne(data);
-        res.send(`se ah ingresado la data`)
+        res.send(`se ha ingresado la data`)
     }catch(Error){ 
         res.status(400).send(Error);
     }
@@ -32,7 +43,7 @@ medicamento.delete('/', async (req,res)=>{
     try {
         let data = req.body
         let id =data._id
-        let funtion = await collection.deleteOne({"_id":id},)
+        let funtion = await collection.deleteOne({"_id":id})
         res.send(funtion)
 
     } catch (error) {
@@ -41,11 +52,11 @@ medicamento.delete('/', async (req,res)=>{
 })
 
 medicamento.put("/", async (req,res)=>{
-    let actualizaciones ={...req.body,caducidad:new Date(req.body.caducidad)};
+    let actualizaciones ={...req.body, caducidad : new Date(req.body.caducidad)};
     let filter = parseInt(req.query.id, 10)
 try{
     let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
-    res.send("se ah actualizado la data")  
+    res.send("se ha actualizado la data")  
 } catch (error) {
     res.send(error);
 }
